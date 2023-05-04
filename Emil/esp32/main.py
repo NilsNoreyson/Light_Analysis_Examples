9a304c6fd3f15aafb30e77c3828944f78322650d
Peter Bonanati <peter.bonanati@gmail.com>
2023-05-04 14:48:30 +0200

    no neo

--- Emil/esp32/main.py
+++ Emil/esp32/main.py
@@ -5,15 +5,23 @@ import neopixel
 
 led = machine.Pin(25, machine.Pin.OUT)
 
-np = neopixel.NeoPixel(machine.Pin(22), 22)
-
-
 while True:
-    for i in range(22):
-        np.fill([0,0,0])
-        np[i] = [255,0,0]
-        np.write()
-        time.sleep(0.1)
+    led.value(1)
+    time.sleep(0.5)
+    led.value(0)
+    time.sleep(0.5)
+
+
+
+# np = neopixel.NeoPixel(machine.Pin(22), 22)
+
+
+# while True:
+#     for i in range(22):
+#         np.fill([0,0,0])
+#         np[i] = [255,0,0]
+#         np.write()
+#         time.sleep(0.1)
 
 
 #upload this with
@@ -21,9 +29,4 @@ while True:
 # led.value(0)
 
 
-# while True:
-#     led.value(1)
-#     time.sleep(0.5)
-#     led.value(0)
-#     time.sleep(0.5)
 
